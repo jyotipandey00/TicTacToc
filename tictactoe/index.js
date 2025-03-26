525c084ebc4d7a712bf5f7096da1c8f96a67ff95
Jyoti Pandey <jyoti.munita@gmail.com>
2025-03-26 20:15:49 +0530

    Add files via upload

---
+++ tictactoe/index.js
@@ -0,0 +1,99 @@
+// console.log("Hello World!");
+
+// //VARIABLES
+// const profile ={
+//     name : "jyoti pandey",
+//     followers : 121,
+//     following : 100,
+//     isFollow : true,
+//     Bio : "Engineering Student",
+// };
+// console.log(profile);
+
+// //Arithmetic operators
+// let a = 10;
+// let b = 2;
+// console.log("a = ",a ,"& b = ",b)
+// console.log("a + b =" ,a+b);
+// console.log("a - b =" ,a-b);
+// console.log("a * b =" ,a*b);
+// console.log("a / b =" ,a/b);
+// console.log("a % b =" ,a%b);
+// console.log("a ** b =" ,a**b);    //exponential, a power b
+
+// //Logical operator
+// let x = 12;
+// let y = 3;
+// let cond1 = x>y;
+// let cond2 = x==y;
+// console.log("x = ",x,"& y = ",y);
+// console.log("cond1 = x>y & cond2 = x==y");
+// console.log("cond1 && cond2 = ",cond1 && cond2);
+// console.log("cond1 || cond2 = ",cond1 || cond2);
+// console.log("!(x<y) = ", !(x<y));
+
+//conditional statement
+// let age = 21;
+// if(age>=18){
+//     console.log("You can Vote");
+// }else{
+//     console.log("You cannot Vote");
+// }
+
+//prompt
+//alert("HELLO!");
+// let number = prompt("Enter a number: ");
+// if(number%5==0){
+//     console.log(number,"is multiple of 5.");
+// }else{
+//     console.log(number,"is NOT multiple of 5.");
+// }
+
+//loops
+// let sum =0;
+// for(let i =1;i<=5;i++){
+//     sum += i;
+//     console.log("Sum = ",sum);
+// }
+// let num = 24;
+// let game = prompt("Guess the number: ");
+// while(num != game){
+//     game = prompt("U entered wrong number, Guess again: ");
+// }
+// console.log("Congrats, U entered right number");
+
+
+//strings
+// let obj = {
+//     item : "ladoo",
+//     price : 50 ,
+// };
+// console.log(`The cost of ${obj.item} is ${obj.price} rupees`);
+
+
+//arrays
+// let arr = [12,23,65,31,21,24];
+// console.log(arr);
+// console.log(arr.length);
+
+// let sum = 0;
+// let marks = [85,97,44,37,76,60];
+// for(let i = 0; i<marks.length;i++){
+//     sum += marks[i];
+// }
+// let avg = sum/marks.length;
+// console.log(avg);
+
+//function
+
+// function countVowels(str){
+//     let count = 0;
+//     for(const char of str){
+//          if(char === "a" || char === "e" || char === "i" || char === "o" || char === "u"){
+//             count++;
+//          }          
+//     }
+//     console.log(count);
+// }
+
+
